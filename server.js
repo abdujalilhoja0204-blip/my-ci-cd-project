96de80a91b9f377d283ff874418ff13a758fc0c2
abdujalil <abdujalilhoja0204@gmail.com>
2025-12-11 12:30:46 +0500

    Update hello world response message

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 
 // --- Hello world route ---
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    res.send('Hello world CI-CD');
 });
 // --------------------------
 
